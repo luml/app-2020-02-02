23e5d926913ee8435cc349150b9ee55f836e7d25
luml <1520622848@qq.com>
2020-02-05 18:05:01 +0800

    .

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,8 +29,9 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 360,
           padding: `0 1.0875rem 1.45rem`,
+          width: `28vw`,
         }}
       >
         <main>{children}</main>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,11 +3,15 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Image from "../components/image"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Feel you on Sundays</h1>
+    <h2>Feel you on Sundays</h2>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
     <Link to="/">⇠Go back</Link>
   </Layout>
 )
