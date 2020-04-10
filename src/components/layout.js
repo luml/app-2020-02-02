3f13938a1a81e896fa4964648589c13779fc28a8
luml <1520622848@qq.com>
2020-04-10 10:58:45 +0800

    feat(apply min-height for main)

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
+        <main style={{ minHeight: "78vh" }}>{children}</main>
         <footer style={{ textAlign: "center" }}>
           © {new Date().getFullYear()}, Built with
           {` `}
